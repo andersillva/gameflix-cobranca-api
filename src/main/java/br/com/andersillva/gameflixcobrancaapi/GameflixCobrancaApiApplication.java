76500b4a6badc36365361db9b5a9900ae2e1bb8d
Ander Sillva <andersillva11@gmail.com>
2022-08-20 14:51:51 -0300

    Criação do repositório.

---
+++ src/main/java/br/com/andersillva/gameflixcobrancaapi/GameflixCobrancaApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.andersillva.gameflixcobrancaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameflixCobrancaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameflixCobrancaApiApplication.class, args);
+	}
+
+}
